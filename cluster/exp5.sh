a505299bac7db0b759217641c4dc41037cbbba13
moucheng2017 <rmapmxu@ucl.ac.uk>
2023-03-29 16:42:58 +0100

    more experiments for task 01

---
+++ cluster/exp5.sh
@@ -0,0 +1,8 @@
+#$ -l tmem=40G
+#$ -l gpu=true
+#$ -S /bin/bash
+#$ -j y
+#$ -l h_rt=140:00:00
+#$ -wd /SAN/medic/PerceptronHead/codes/SatsumaSeg/
+
+~/miniconda3/envs/pytorch1.4/bin/python Main.py -c configs/exp5.yaml
